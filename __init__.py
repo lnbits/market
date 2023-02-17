d6f800b6f69cd0339c481bac8d4b7ba4ba66bd02
dni ⚡ <office@dnilabs.com>
2023-02-17 11:34:16 +0100

    fix stop tasks pr from vlad

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ import asyncio
 
 from fastapi import APIRouter
 from starlette.staticfiles import StaticFiles
+from typing import List
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
@@ -37,7 +38,9 @@ from .tasks import wait_for_paid_invoices
 from .views import *  # noqa: F401,F403
 from .views_api import *  # noqa: F401,F403
 
+scheduled_tasks: List[asyncio.Task] = []
 
 def market_start():
     loop = asyncio.get_event_loop()
-    loop.create_task(catch_everything_and_restart(wait_for_paid_invoices))
+    task = loop.create_task(catch_everything_and_restart(wait_for_paid_invoices))
+    scheduled_tasks.append(task)
